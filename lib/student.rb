605c6d1d51ea45c6a8fcb19ede8ce020927700a5
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-11-13 02:05:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,8 +32,8 @@ class Student
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
-    binding.pry
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    binding.pry
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
