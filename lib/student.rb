c14e8e7c328cc9831817b37eff7a694471f54d63
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-11-13 01:56:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,6 +24,10 @@ class Student
     SQL
     DB[:conn].execute(sql)
   end
+
+  def save
+    self.new(name: name, grade: grade)
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
